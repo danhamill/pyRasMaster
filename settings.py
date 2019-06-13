acd04f40d01bd0a57ceb9dc003423e50d3d22f5f
Daniel Hamill <hamill.daniel@gmail.com>
2019-06-13 17:11:01 -0400

    working on output vatiables module for rascontroller, right now its hard coded in rascontrol.rascontrol

---
+++ settings.py
@@ -0,0 +1,14 @@
+import logging as lg
+
+logs_folders = 'logs'
+
+
+# cache server responses
+use_cache = False
+
+# write log to file and/or to console
+log_file = False
+log_console = False
+log_level = lg.INFO
+log_name = 'test_repo_fun'
+log_filename = 'test_repo_fun'
